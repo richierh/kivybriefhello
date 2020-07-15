b43029ce4d11b140bcf289f0eacb732a0995a2a6
Your Name <you@example.com>
2020-07-15 22:01:15 +0200

    a

---
+++ src/helloworld/app.py
@@ -0,0 +1,17 @@
+"""
+My first application
+"""
+import sys
+import os
+print(sys.path)
+os.environ["KIVY_GL_BACKEND"]= "angle_sdl2"
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+class MyApp(App):
+	def build(self):
+		return Widget()
+def main():
+    # This should start and launch your app!
+    ap = MyApp()
+    ap.run()
