8d067adef7c052680413e8d1ed3c476c4efec361
Richie Rahmat Hidayat <pmc.computer@yahoo.com>
2021-01-19 20:09:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ print(sys.path)
 os.environ["KIVY_GL_BACKEND"]= "angle_sdl2"
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
 
 class MyApp(App):
 
     def build(self):
-        return Label(text='Hello world')
+        return Button(text='Hello world')
 
 def main():
     # This should start and launch your app!
