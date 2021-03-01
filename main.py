81199d3f70336c40f6b255fe4b6dff28b0d46cdf
Richie Rahmat Hidayat <pmc.computer@yahoo.com>
2021-03-01 20:06:28 +0700

    Update main.py
    hhs

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 from kivy.uix.widget import Widget
 from kivymd.app import MDApp
 from webview import WebView
@@ -26,27 +26,3 @@ class MyWebApp(MDApp):
 
 if __name__ == '__main__':
     MyWebApp().run
-=======
-
-"""
-My first application
-"""
-import sys
-import os
-print(sys.path)
-os.environ["KIVY_GL_BACKEND"]= "angle_sdl2"
-from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.uix.button import Button
-
-class MyApp(App):
-
-    def build(self):
-        return Button(text='Hello world')
-
-def main():
-    # This should start and launch your app!
-    ap = MyApp()
-    ap.run()
-main()
->>>>>>> 8d067adef7c052680413e8d1ed3c476c4efec361
