40760b0a52135a636440fd2d63332bbdc2b1764e
Richie Rahmat Hidayat <pmc.computer@yahoo.com>
2021-03-02 17:13:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -25,25 +25,3 @@ class MyWebApp(MDApp):
 
 if __name__ == '__main__':
     MyWebApp().run
-
-"""
-My first application
-"""
-import sys
-import os
-print(sys.path)
-os.environ["KIVY_GL_BACKEND"]= "angle_sdl2"
-from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.uix.button import Button
-
-class MyApp(App):
-
-    def build(self):
-        return Button(text='Hello world')
-
-def main():
-    # This should start and launch your app!
-    ap = MyApp()
-    ap.run()
-main()
